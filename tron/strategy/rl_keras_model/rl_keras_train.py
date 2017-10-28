9bc1f8569b7e7e18bba42923f9a0cb06ac268f9f
David Gengenbach <davidgengenbach@gmx.de>
2017-10-28 20:33:30 +0200

    Added keras rl strategy

---
+++ tron/strategy/rl_keras_model/rl_keras_train.py
@@ -0,0 +1,36 @@
+import os
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.optimizers import Adam
+
+from rl.agents.dqn import DQNAgent
+from rl.policy import EpsGreedyQPolicy
+from rl.memory import SequentialMemory
+
+TRAIN_FILE = 'tmp/rl_keras.npy'
+
+def load_progress(model):
+    if not os.path.exists(TRAIN_FILE):
+        print('File not existing: {}'.format(TRAIN_FILE))
+        return
+    model.load_weights(TRAIN_FILE)
+
+def save_progress(model):
+    model.save_weights(TRAIN_FILE, overwrite=True)
+
+def get_model(env):
+    num_actions = len(env.get_available_actions())
+    model = Sequential()
+    model.add(Flatten(input_shape=(1,) + env.game_field.shape))
+    model.add(Dense(16))
+    model.add(Activation('relu'))
+    model.add(Dense(num_actions))
+    model.add(Activation('linear'))
+    print(model.summary())
+
+    policy = EpsGreedyQPolicy()
+    memory = SequentialMemory(limit=50000, window_length=1)
+    dqn = DQNAgent(model=model, nb_actions=num_actions, memory=memory, nb_steps_warmup=1000,
+    target_model_update=1e-2, policy=policy)
+    dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+    return dqn
---
+++ tron/strategy/rl_keras_strategy.py
@@ -0,0 +1,17 @@
+from . import player_game
+from .rl_keras_model import rl_keras_train
+
+class RLKerasStrategy(player_game.PlayerStrategy):
+
+    def __init__(self, player_idx = None, game = None):
+        super(RLKerasStrategy, self).__init__(player_idx)
+        self.game = game
+        self.load_model()
+
+    def load_model(self):
+        self.model = rl_keras_train.get_model(self.game)
+        rl_keras_train.load_progress(self.model)
+
+    def get_action(self, game, game_state, other=None):
+        action = self.model.forward(game_state.game_field)
+        return action
