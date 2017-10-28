5a6b469888e7c78e939c3e0a46ce01bf18ad7299
andreas <andyfr@gmx.de>
2017-10-28 15:10:11 +0200

    POC

--- beste_ki/beste_ki.py
+++ beste_ki/beste_ki.py
@@ -1,4 +1,14 @@
 import random
 
-def predict(game):
-    return random.randint(0, 2)
+
+class Beste_ki(object):
+
+    def __init__(self, player):
+        self.player = player
+
+    def predict(self, game):
+        print(self.has_lost(game))
+        return random.randint(0, 2)
+
+    def has_lost(self, game):
+        return game.player_lost[self.player]
